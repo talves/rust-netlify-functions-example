6500057ccc9a726aa7d6cb38315b59e30cfca88d
talves <tony@3alves.com>
2022-07-26 14:19:02 -0700

    fix: remove functions dir

--- netlify/functions-src/hello/src/main.rs
+++ netlify/functions-src/hello/src/main.rs
@@ -1,21 +1,28 @@
-use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use http::header::HeaderMap;
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{service_fn, Context, Error, LambdaEvent};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
 
     let handler = service_fn(handler);
     lambda_runtime::run(handler).await?;
     Ok(())
 }
 
-async fn handler(event: LambdaEvent<ApiGatewayProxyRequest>) -> Result<ApiGatewayProxyResponse, Error> {
-    let (event, _ctx ) = event.into_parts();
+async fn handler(
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let (event, _ctx) = event.into_parts();
+    let event: ApiGatewayProxyRequest = event;
+    let _ctx: Context = _ctx;
     let path = event.path.unwrap();
 
     let resp = ApiGatewayProxyResponse {
