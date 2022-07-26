d04811c203fbe9704fd7fcc3f39faab25d654f5b
talves <tony@3alves.com>
2022-07-26 14:50:28 -0700

    update: move build to CI on Netlify

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-cargo build --release
-cp target/release/hello netlify/functions/hello
+yarn global add @ziglang/cli
+whereis zig
+cargo install cargo-lambda
+# mkdir -p netlify/functions
+# cargo build --release
+cargo lambda build --release
+# cp target/lambda netlify/functions
