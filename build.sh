85f3042faf67aae45da48fd6b00155e61dde3d06
talves <tony@3alves.com>
2022-07-26 16:01:51 -0700

    update: use zibuild crate for brevity

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 
 which zig || yarn global add @ziglang/cli
 # /opt/buildhome/.yarn/bin/zig
-which cargo-lambda || cargo install cargo-lambda
+which cargo-zigbuild || cargo install cargo-zigbuild
 mkdir -p netlify/functions
 # cargo build --release
-cargo lambda build --release
+cargo zigbuild --target x86_64-unknown-linux-gnu.2.26 --release
 # add copy commands as you add functions
-cp target/lambda/hello/bootstrap netlify/functions/hello
+cp target/x86_64-unknown-linux-gnu/release/hello netlify/functions/
