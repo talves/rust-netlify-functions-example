696948d5d945cae6bb0af474566f25bb686384b5
talves <tony@3alves.com>
2022-07-26 15:40:00 -0700

    fix: reverse check

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-which zig && yarn global add @ziglang/cli
+which zig || yarn global add @ziglang/cli
 # /opt/buildhome/.yarn/bin/zig
-which cargo-lambda && cargo install cargo-lambda
+which cargo-lambda || cargo install cargo-lambda
 mkdir -p netlify/functions
 # cargo build --release
 cargo lambda build --release
