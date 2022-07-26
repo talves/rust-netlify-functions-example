b093f6f15f4af40644c8a82c431c0917740e4a1f
talves <tony@3alves.com>
2022-07-26 15:35:16 -0700

    fix: only install if not cached already

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-yarn global add @ziglang/cli
-whereis zig
-cargo install cargo-lambda
+which zig && yarn global add @ziglang/cli
+# /opt/buildhome/.yarn/bin/zig
+which cargo-lambda && cargo install cargo-lambda
 mkdir -p netlify/functions
 # cargo build --release
 cargo lambda build --release
