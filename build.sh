93d9fef1321f722f75295fa0fb03224fe1870b6b
talves <tony@3alves.com>
2022-07-26 17:50:10 -0700

    fix: copy to functions

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ mkdir -p netlify/functions
 cargo zigbuild --target x86_64-unknown-linux-gnu.2.26 --release
 # add copy commands as you add functions
 cp target/x86_64-unknown-linux-gnu/release/hello netlify/functions/
+cp target/x86_64-unknown-linux-gnu/release/dad-joke netlify/functions/
