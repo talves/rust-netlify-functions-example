6c9ba6143bcfe9ae129719649a315ac96ebd8c45
talves <tony@3alves.com>
2022-07-27 09:04:53 -0700

    update: target x86_64-unknown-linux-musl

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ set -euxo pipefail
 mkdir -p netlify/functions
 cargo build --target x86_64-unknown-linux-musl --release
 # add copy commands as you add functions
-cp target/release/hello netlify/functions/
-cp target/release/dad-joke netlify/functions/
+cp target/x86_64-unknown-linux-musl/release/hello netlify/functions/
+cp target/x86_64-unknown-linux-musl/release/dad-joke netlify/functions/
