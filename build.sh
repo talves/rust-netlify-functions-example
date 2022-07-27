56b59d8fb57ab4fc6e4be7cd58dd9c002ebdf8b9
talves <tony@3alves.com>
2022-07-27 08:51:00 -0700

    update: build environment fixed by Netlify

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-which zig || yarn global add @ziglang/cli
-# /opt/buildhome/.yarn/bin/zig
-which cargo-zigbuild || cargo install cargo-zigbuild
+# which zig || yarn global add @ziglang/cli
+# # /opt/buildhome/.yarn/bin/zig
+# which cargo-zigbuild || cargo install cargo-zigbuild
+# mkdir -p netlify/functions
+# cargo zigbuild --target x86_64-unknown-linux-gnu.2.26 --release
+# # add copy commands as you add functions
+# cp target/x86_64-unknown-linux-gnu/release/hello netlify/functions/
+# cp target/x86_64-unknown-linux-gnu/release/dad-joke netlify/functions/
+
 mkdir -p netlify/functions
-cargo zigbuild --target x86_64-unknown-linux-gnu.2.26 --release
+cargo build --release
 # add copy commands as you add functions
-cp target/x86_64-unknown-linux-gnu/release/hello netlify/functions/
-cp target/x86_64-unknown-linux-gnu/release/dad-joke netlify/functions/
+cp target/release/hello netlify/functions/
+cp target/release/dad-joke netlify/functions/
